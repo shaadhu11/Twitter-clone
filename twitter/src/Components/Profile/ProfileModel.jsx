05fae6e81e1fa6ccf55e619bbc0080636263f136
shaadhu11 <mhdshahad11@gmail.com>
2025-02-15 15:41:01 +0530

    Subscription model finished

--- twitter/src/Components/Profile/ProfileModel.jsx
+++ twitter/src/Components/Profile/ProfileModel.jsx
@@ -12,7 +12,7 @@ const style = {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: 600,
-    bgcolor: "white",
+    bgcolor: "background.paper",
     border: "none",
     boxShadow: 24,
     p: 4,
